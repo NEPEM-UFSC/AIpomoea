afbbd6bc3dc4ce4f26f60617bde93fa2260a5a96
Matheus L Machado <matheuslmachado03@gmail.com>
2024-10-15 19:30:34 -0300

    refactor: update file handling to use WebUtils instead of HTML5 File API - Replaced usage of HTML5 File API's file path access with WebUtils.getPathForFile for improved security and compatibility with Electron 33.0.0.                 - Updated preload.js to expose WebUtils in the renderer process. - Ensured compatibility with the new Electron version by modifying file handling logic.

--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, webUtils } = require('electron');
 
 contextBridge.exposeInMainWorld('ipcRenderer', {
     send: (channel, data) => ipcRenderer.send(channel, data),
     on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(event, ...args))
+});
+
+contextBridge.exposeInMainWorld('webUtils', {
+    getPathForFile: async (file) => webUtils.getPathForFile(file)
 });
