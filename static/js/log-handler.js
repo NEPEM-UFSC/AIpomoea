849410b7669d3bbac5aa78d21bdfd056e3b60961
Matheus L Machado <matheuslmachado03@gmail.com>
2024-11-17 13:37:23 -0300

    Refatorar: Adicionar documentação para a função logMessage no log-handler.js

--- static/js/log-handler.js
+++ static/js/log-handler.js
@@ -1,3 +1,15 @@
+/**
+ * Sends a log message to the IPC renderer or logs an error if unavailable.
+ * @example
+ * logMessage('info', 'This is an informational message')
+ * // If ipcRenderer is available, message is sent else logs error.
+ * @param {string} level - The severity level of the log message ('info', 'warn', 'error').
+ * @param {string} message - The content of the log message.
+ * @returns {void} Does not return a value.
+ * @description
+ *   - Uses 'ipcRenderer' to send log messages.
+ *   - Provides fallback error logging if 'ipcRenderer' is not present.
+ */
 function logMessage(level, message) {
     if (ipcRenderer) {
         ipcRenderer.send('log-message', level, message);
