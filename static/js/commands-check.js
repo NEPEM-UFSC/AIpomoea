7f84f0a16b7154555a9058bc81cfa731848f2264
Matheus L Machado <matheuslmachado03@gmail.com>
2024-11-20 18:07:56 -0300

    Refatorar: Adicionar documentação para a função checkAndBlockOperations em commands-check.js

--- static/js/commands-check.js
+++ static/js/commands-check.js
@@ -1,3 +1,15 @@
+/**
+ * Disables checkboxes for operations not found in the specified model arrays.
+ * @example
+ * checkAndBlockOperations()
+ * // Fetches data from models.json, disables unavailable operations
+ * @param {void} No parameters required for this function.
+ * @returns {void} This function does not return any value.
+ * @description
+ *   - Fetches a JSON file to determine which operations should remain enabled.
+ *   - Disables operations that are not listed in `root` or `leaves` arrays from the fetched data.
+ *   - Logs an error to the console if there is an issue loading the JSON file.
+ */
 function checkAndBlockOperations() {
     fetch('../models.json')
         .then(response => response.json())
